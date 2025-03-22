786334f8fec6fc47e4b762e1eab58c1113c5982d
sonalpatil87 <sonalpatil871987@gmail.com>
2025-03-22 09:40:18 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+a=23
+b=78
+add=a+b
+sub=a-b
+div=a*b
+multi=a/b
+print("addition of",a,"and",b,"is",add)
+print("Subtration of",a,"and",b,"is",sub)
+print("division of",a,"and",b,"is",div)
+print("multiplication of",a,"and",b,"is",div)
